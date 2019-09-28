4340a16071c039529612d866f928cd95b0cf5746
Anastasiia Generalova <rhinrei@gmail.com>
2019-09-28 21:31:04 +0300

    added: substraction, division and multiplication;clear function, numbers with more than 1 digit

--- src/main.ts
+++ src/main.ts
@@ -6,27 +6,25 @@ document.addEventListener('DOMContentLoaded', function () {
     const resultBlock = document.getElementById('result');
 
     // создадим объект "калькулятор"
-    const calc: Calculator = new Calculator();
+    const calc: Calculator = new Calculator(printResult);
 
     /**
      * Функция для вывода результата
      * @param value
      */
-    function printResult(value: number) {
-        resultBlock.innerText = '' + value;
+    function printResult(value: string) {
+        resultBlock.innerText = value;
     }
 
     // найдём все кнопки с числами
+
     for (let i = 0; i < 10; i++) {
         let btn = document.getElementById('btn' + i);
+        let inputNumber = i.toString();
 
         btn.onclick = function () {
             // передаём в калькулятор нажатое число
-            calc.apply(i);
-            // сбрасываем операцию
-            calc.operation = null;
-            // выводим результат
-            printResult(calc.currentResult);
+            calc.enterNumber(i);
         };
 
     }
@@ -34,7 +32,25 @@ document.addEventListener('DOMContentLoaded', function () {
     // кнопка "плюс"
     document.getElementById('plus').onclick = function () {
         // передаём в калькулятор выбранную операцию
-        calc.operation  = Operation.SUM;
+        calc.apply(Operation.SUM);
     };
 
+    document.getElementById('minus').onclick = function () {
+        // передаём в калькулятор выбранную операцию
+        calc.apply(Operation.SUB);
+
+    };
+    document.getElementById('mult').onclick = function () {
+        // передаём в калькулятор выбранную операцию
+        calc.apply(Operation.MUL);
+    };
+    document.getElementById('div').onclick = function () {
+        // передаём в калькулятор выбранную операцию
+        calc.apply(Operation.DIV);
+    };
+
+    document.getElementById('clear-button').onclick = function () {
+        calc.clear();
+        printResult('');
+    };
 });
